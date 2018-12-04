decd8ace26d4fae5df8212572c8c52078e65a0db
Afrish Khan S <ryanafrish7@gmail.com>
2018-12-05 00:35:28 +0530

    Add ar-datepicker-css file

---
+++ ar-datepicker-css.js
@@ -0,0 +1,7 @@
+import { html } from "@polymer/lit-element"; 
+
+export const style = html`
+    <style>
+        
+    </style>
+`;
--- ar-datepicker.js
+++ ar-datepicker.js
@@ -1,12 +1,18 @@
 import { LitElement, html } from "@polymer/lit-element"; 
+import { style } from "./ar-datepicker-css";
 
 /**
 * @polymer
 * @extends HTMLElement
 */
 export class Datepicker extends LitElement {
+    renderStyle() {
+        return style;
+    }
+
     render() {
         return html`
+            ${this.renderStyle()}
             <p>I will be a datepicker one day</p>
         `;
     }
